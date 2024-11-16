d2faadee6ee0f99b81cebffa6a5c6026eef6077b
abdo <you@example.com>
2024-11-16 10:52:48 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Title from './title'
+import './index.css'
+function App() {
+
+  return (
+    <Title/>
+  )
+}
+
+export default App
---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+function Title() {
+    return(
+        <div className="div1">
+            <h1>Test project</h1>
+        </div>
+    );
+}
+
+export default Title;
